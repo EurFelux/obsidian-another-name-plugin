b082a152eac775cef2455aa0fb8208d5c01b54fc
zjphantom <eurfelux@gmail.com>
2023-09-20 14:04:09 +0800

    0.1.0

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, MarkdownFileInfo, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, CachedMetadata } from 'obsidian';
+import { App, Editor, MarkdownView, Events, EventRef, Plugin, PluginSettingTab, Setting, TFile, CachedMetadata, MarkdownPostProcessorContext, WorkspaceLeaf } from 'obsidian';
 // Remember to rename these classes and interfaces!
 
 interface AnotherNameSettings {
@@ -11,29 +11,33 @@ const DEFAULT_SETTINGS: AnotherNameSettings = {
 
 export default class AnotherNamePlugin extends Plugin {
 	settings: AnotherNameSettings;
+	reloadForFile: (file: TFile, cache: CachedMetadata, leaf?: WorkspaceLeaf, inlineTitle?: HTMLElement) => void;
+	reloadAllVisible: () => void;
 
-	async onload() {
-		await this.loadSettings();
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new AnotherNameSettingTab(this.app, this));
-
-		const reloadForFile = (file: TFile, cache: CachedMetadata) => {
-			// 检查是否有旧的元素
+	async initReloadFunction() {
+		this.loadSettings();
+		this.reloadForFile = (file: TFile, cache: CachedMetadata, leaf?: WorkspaceLeaf) => {
 			const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
-			const editor = mdView?.editor;
-			const cmEditorEl = editor?.containerEl;
+			let viewContentEl = leaf?.view?.contentEl;
+			if (!viewContentEl)
+				viewContentEl = mdView?.contentEl;
 
-			const anotherNameElOld = cmEditorEl?.querySelectorAll('.another-name');
-			// if (anotherNameElOld) {
-			// 	anotherNameElOld.remove();
-			// }
+			let inlineTitle = leaf?.view.inlineTitleEl;
+			if (!inlineTitle)
+				inlineTitle = mdView?.inlineTitleEl;
+
+			// console.log('reloadForFile', file, cache, mdView, inlineTitle);
+			// console.log('viewContentEl', viewContentEl);
+			// 检查是否有旧的元素
+			const anotherNameElOld = viewContentEl?.querySelector('.another-name');
 			if (anotherNameElOld) {
-				anotherNameElOld.forEach((el: Element) => {
-					el.remove();
-				})
+				anotherNameElOld.remove();
+				// anotherNameElOld.forEach((el: HTMLElement) => {
+				// 	el.remove();
+				// });
 			}
 
+			if (!inlineTitle) return;
 
 			// 读取元数据
 			const frontmatter = cache?.frontmatter;
@@ -42,9 +46,11 @@ export default class AnotherNamePlugin extends Plugin {
 			let anotherName;
 			if (frontmatter) {
 				anotherName = frontmatter[this.settings.propertyName]
-				console.log(anotherName);
+				if (!anotherName)
+					return;
+				// console.log(anotherName);
 			} else {
-				console.log('no frontmatter')
+				// console.log('no frontmatter')
 				return;
 			}
 
@@ -54,27 +60,52 @@ export default class AnotherNamePlugin extends Plugin {
 			anotherNameEl.classList.add('another-name');
 
 			// 插入
-			if (mdView) {
-				// console.log(editor)
-				const inlineTitle = cmEditorEl.querySelectorAll('.inline-title');
-
-				if (inlineTitle) {
-					// inlineTitle.parentNode.insertBefore(anotherNameEl, inlineTitle.nextSibling);
-					inlineTitle.forEach((el: HTMLElement) => {
-						el.style.marginBottom = '0px';
-						el.parentNode?.insertBefore(anotherNameEl, el.nextSibling);
-					})
-				}
-
+			if (inlineTitle) {
+				// console.log('inlineTitle', inlineTitle);
+				inlineTitle.style.marginBottom = "0px";
+				inlineTitle.parentNode?.insertBefore(anotherNameEl, inlineTitle.nextSibling);
 			}
+
 		}
+
+		this.reloadAllVisible = () => {
+			this.app.workspace.iterateRootLeaves(leaf => {
+				// console.log("It works.")
+				const display = leaf.containerEl.style.display;
+				// console.log("leaf", leaf)
+				// console.log("display", display)
+				if (display === "none") return;
+				const file = leaf.view.file;
+				if (!file) return;
+				const cache = this.app.metadataCache.getFileCache(file);
+				if (cache) {
+					this.reloadForFile(file, cache, leaf);
+				}
+			});
+		};
+	}
+
+
+	async onload() {
+		await this.loadSettings();
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new AnotherNameSettingTab(this.app, this));
+
+		this.initReloadFunction();
+
+		// this.app.workspace.onLayoutReady(() => {
+		// 	this.reloadAllVisible();
+		// });
+
+		let leaves = this.app.workspace.getLeavesOfType('markdown');
 
 		// add something!
 		this.registerEvent(this.app.workspace.on('file-open', (file: TFile) => {
 			if (file) {
 				const cache = this.app.metadataCache.getFileCache(file);
 				if (cache) {
-					reloadForFile(file, cache);
+					this.reloadForFile(file, cache);
 				}
 			}
 
@@ -83,8 +114,14 @@ export default class AnotherNamePlugin extends Plugin {
 		this.registerEvent(this.app.metadataCache.on('changed', (file: TFile, data: string, cache: CachedMetadata) => {
 			// console.log('changed', file, data, cache);
 			if (file) {
-				reloadForFile(file, cache);
+				this.reloadForFile(file, cache);
 			}
+		}));
+
+		this.registerEvent(this.app.workspace.on('layout-change', () => {
+			// 这样效率很低，但是我没有更好的办法
+			// console.log('trigger layout-change')
+			this.reloadAllVisible();
 		}));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
@@ -109,6 +146,11 @@ export default class AnotherNamePlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async updateSettings() {
+		await this.loadSettings();
+
+	}
 }
 
 // Settings tab
@@ -127,13 +169,15 @@ class AnotherNameSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('Property name')
-			.setDesc('')
+			.setDesc('wait a while to take effect')
 			.addText(text => text
 				.setPlaceholder('another name')
 				.setValue(this.plugin.settings.propertyName)
 				.onChange(async (value) => {
 					this.plugin.settings.propertyName = value;
 					await this.plugin.saveSettings();
+					this.plugin.initReloadFunction();
+
 				}));
 	}
 }
