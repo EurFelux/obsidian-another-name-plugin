5d0ebc78f7a94fac6a2e0630bfa4f0433d6ac143
1C0D <1conscience@gmx.fr>
2023-12-04 06:49:23 +0100

    condition if no frontmatter

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,7 @@ export default class AnotherNamePlugin extends Plugin {
 	getSubTitle() {
 		const file = this.app.workspace.getActiveFile()
 		const metadataCache = this.app.metadataCache.getCache(file?.path as string)
+		if( !('frontmatter' in metadataCache!)) return false
 		// use aliases or own property
 		if (this.settings.useAliases) {
 			this.activeSubTitle = metadataCache?.frontmatter!["aliases"]
@@ -47,10 +48,12 @@ export default class AnotherNamePlugin extends Plugin {
 			const settingName = this.settings.propertyName
 			this.activeSubTitle = metadataCache?.frontmatter![settingName]
 		}
+		return true
 	}
 
 	async addSubTitleEl() {
-		this.getSubTitle();
+		const ret = this.getSubTitle();
+		if(!ret) return
 		let activeView = this.app.workspace.getLeaf(false)?.view;
 		const inlineTitle = activeView?.containerEl.querySelector("div.inline-title") as HTMLElement;
 
