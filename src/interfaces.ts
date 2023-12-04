670e76c8765844174f63fbb44e41553a8f32c03a
1C0D <1conscience@gmx.fr>
2023-12-04 05:59:06 +0100

    total refactoring and fix

---
+++ src/interfaces.ts
@@ -0,0 +1,9 @@
+export interface AnotherNameSettings {
+    propertyName: string;
+    useAliases: boolean
+}
+
+export const DEFAULT_SETTINGS: AnotherNameSettings = {
+    propertyName: 'another-name',
+    useAliases: true
+}
