c487fdd89e9a25164508305d5a3eec8563af57d6
zjphantom <eurfelux@gmail.com>
2025-04-27 23:11:41 +0800

    update main.ts, remove unnecessary comments; update manifest.json, deleted fundingUrl.

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import {
     FileView,
     Notice,
 } from "obsidian";
-// Remember to rename these classes and interfaces!
+
 
 interface AnotherNameSettings {
     propertyName: string;
@@ -122,15 +122,6 @@ export default class AnotherNamePlugin extends Plugin {
 				this.app.workspace.iterateAllLeaves(this.reloadLeaf);
             })
         );
-
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-        // 	// console.log('click', evt);
-        // });
-
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
     }
 
     onunload() {}
